3ad5677b1b4ce810907c73179f4bb2a0daedba11
susegitro <susero@posteo.de>
2019-10-27 14:36:43 +0100

    Single Blog Layout

--- single.php
+++ single.php
@@ -9,20 +9,29 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area-full">
 		<main id="main" class="site-main" role="main">
 
 		<?php
 		while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', get_post_format() );
+			get_template_part( 'template-parts/content', get_post_format() ); ?>
+			
+			<div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <?php the_post_navigation();
+                        
+                        // If comments are open or we have at least one comment, load up the comment template.
+                        if ( comments_open() || get_comments_number() ) :
+                            comments_template();
+                        endif; ?>
+                    </div><!-- col -->
+                </div><!-- row -->
+            </div><!-- container -->
+            
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
+		<?php endwhile; // End of the loop.
 		?>
 
 		</main><!-- #main -->
